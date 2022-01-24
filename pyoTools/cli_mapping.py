e9aba53ab7c7654042e39286d9a1a8d8475c2792
Ali Allaoui <ali.allaoui@lam.fr>
2022-01-24 21:55:29 +0100

    connect to miPhy by jack

--- pyoTools/cli_mapping.py
+++ pyoTools/cli_mapping.py
@@ -13,19 +13,18 @@ def mapping():
     parser.add_argument('--values', '-v')
 
     args = parser.parse_args()
-    mapping = Mapping(args.mapping, args.values)
+    mappings = Mapping(args.mapping, args.values)
 
-    s = Server(audio='jack', nchnls=mapping.nb_input_channels, ichnls=2)
+    s = Server(audio='jack', nchnls=mappings.nb_input_channels, ichnls=2)
     jclient = jack.Client("mapping")
     pm_list_devices()
     s.setMidiInputDevice(99)  # Open all input devices.
     s.boot()
-    mapping.load()
-
-    # for pyout in jclient.get_ports('pyo:output_*'):
-    #    pyout.disconnect()
-
+    mappings.load()
     jclient.disconnect('pyo:output_1', 'system:playback_1')
     jclient.disconnect('pyo:output_2', 'system:playback_2')
+    for m in mappings.mappings:
+        if "channel" in m.keys():
+            jclient.connect('pyo:output_' + str(m["channel"] + 1), 'miPhy:Input_' + str(m["channel"] + 1))
 
     s.gui(locals())
